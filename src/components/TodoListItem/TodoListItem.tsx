07197ad7b1835a51bd71797679c235875db480bb
SkylineHR31 <skyline_hr_31@outlook.com>
2021-11-24 16:25:36 +0200

    added tsx components and rewrite app file

---
+++ src/components/TodoListItem/TodoListItem.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TodoListItem: React.FC = () => {
+    return (
+        <li className="todo-list-item">
+            
+        </li>
+    );
+  }
+  
+  export default TodoListItem;
