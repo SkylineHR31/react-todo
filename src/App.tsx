6266710e0145099182fb650b1890140004bc8cbd
SkylineHR31 <skyline_hr_31@outlook.com>
2021-11-27 11:05:12 +0200

    added components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+// components
+import TodoList from "./components/TodoList/TodoList";
+
+// styles
+import "./styles/layout.scss";
+import "./styles/todo.scss";
+
+// images
+
+export const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="app">
+      <div className="app-layout">
+        <div className="app-container">
+          <div className="todo-app">
+            <header className="todo-app-header">
+              <h1 className="title">To do app</h1>
+            </header>
+            <div className="todo-app-body">
+              <div className="todo-list-wrapper">
+                <TodoList></TodoList>
+              </div>
+            </div>
+            <footer className="todo-app-footer"></footer>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
 
---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TodoList: React.FC = () => {
+    return (
+        <ul className="todo-list">
+            
+        </ul>
+    );
+  }
+  
+  export default TodoList;
